4583f5991c9f62586ed57ebe6863b1b6ad1d29a4
Debjyoti Chowdhury <debjyotichowdhury@Debjyotis-MacBook-Air.local>
2019-08-28 14:15:02 +0530

    update api with key, value pair

--- IoT_server_restAPI/restapi_database.py
+++ IoT_server_restAPI/restapi_database.py
@@ -1,18 +1,20 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from sqlalchemy import create_engine
 import datetime
-
+import secrets
 
 db_connect = create_engine('sqlite:///database.db')
 app = Flask(__name__)
 api = Api(app)
 
+api_key = secrets.token_urlsafe(16).upper()  # generate API key
 
-class add_data(Resource):
-    def get(self, entry_val):
-        conn = db_connect.connect()
-        conn.execute("INSERT INTO sensor_values (created_at,entry_id,field) VALUES(?, ?, ?)", (str(datetime.datetime.now()), '1', entry_val))
+# class add_data(Resource):
+#     def get(self, entry_val):
+#         conn = db_connect.connect()
+#         conn.execute("INSERT INTO sensor_values (created_at,field) VALUES(?, ?)", (str(datetime.datetime.now()), entry_val))
+#         return '256'
 
 
 class show_data(Resource):
@@ -23,8 +25,23 @@ class show_data(Resource):
         return {"feeds": [{"created_at": data[0], "entry_id": data[1], "fields": data[2]}]}
 
 
-api.add_resource(add_data, '/update/field=<entry_val>')
-api.add_resource(show_data, '/fields')
+class add_data(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('api_key', type=str)
+        parser.add_argument('field', type=str)
+        data = parser.parse_args()
+        if str(data['api_key']).upper() == api_key:
+            conn = db_connect.connect()
+            conn.execute("INSERT INTO sensor_values (created_at,field) VALUES(?, ?)",
+                     (str(datetime.datetime.now()), data['field']))
+        return 256
+
+
+# api.add_resource(add_data, '/update/field=<entry_val>') # old api method
+api.add_resource(show_data, '/feeds')
+api.add_resource(add_data, '/update', endpoint='update')
 
 if __name__ == '__main__':
+    print("Your API key is:", api_key)
     app.run()
