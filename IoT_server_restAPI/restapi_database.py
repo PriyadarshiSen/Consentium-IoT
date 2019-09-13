10d3b1fd1d814937a1bf9d507585837c12d12ab8
Debjyoti Chowdhury <deb@MacBookAir-3.local>
2019-09-13 10:12:21 +0530

    added MQTT support

--- IoT_server_restAPI/restapi_database.py
+++ IoT_server_restAPI/restapi_database.py
@@ -1,15 +1,46 @@
+# ------------------------------------------
+# --- Author: Debjyoti Chowdhury
+# --- Date: 12th September 2019
+# --- Version: 1.0
+# --- Python Ver: 3.6
+# --- Details At: https://iotbytes.wordpress.com/store-mqtt-data-from-sensors-into-sql-database/
+# ------------------------------------------
+
+# ====================================================
+# import statements
+import paho.mqtt.client as mqtt
 from flask import Flask, render_template
 from flask_restful import Resource, Api, reqparse
 from sqlalchemy import create_engine
 import datetime
 import secrets
 
-db_connect = create_engine('sqlite:///database.db')
+# ====================================================
+
+
+db_connect = create_engine('sqlite:////Users/deb/PycharmProjects/consentium_iot/database.db')
+# db_connect = create_engine('sqlite:////home/pi/Consentium-IoT/IoT_server_restAPI/database.db') # for pi
 app = Flask(__name__)
 api = Api(app)
 
 api_key_write = secrets.token_urlsafe(16).upper()  # generate write API key
 api_key_read = secrets.token_urlsafe(16).upper()  # generate write API key
+
+topic = "/ic/nm/serialdata/uno/"
+broker_url = "test.mosquitto.org"
+broker_port = 1883
+
+
+def on_connect(client, userdata, rc):
+    print("Connected With Result Code " + rc)
+
+
+def on_message(client, userdata, message):
+    data = message.payload.decode()
+    topic = message.topic
+    conn = db_connect.connect()
+    conn.execute("INSERT INTO mqtt_sensor_values (created_at, topic_name, field) VALUES(?, ?, ?)",
+                 (str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')), topic, int(data)))
 
 
 @app.route("/")
@@ -21,13 +52,17 @@ def index():
 def things():
     keys = [api_key_write, api_key_read]
     conn = db_connect.connect()  # connect to database
-    query = conn.execute("select * from sensor_values")  # This line performs query and returns json result
-    data = query.cursor.fetchall()
-    raw_data = list(data[len(data) - 1])
-    return render_template('elements.html', write=keys[0], read=keys[1], time=raw_data[0], data=raw_data[2])
+    query_1 = conn.execute("select * from sensor_values")  # This line performs query and returns json result
+    data_1 = query_1.cursor.fetchall()
+    raw_data_1 = list(data_1[len(data_1) - 1])
+    query_2 = conn.execute("select * from mqtt_sensor_values")  # This line performs query and returns json result
+    data_2 = query_2.cursor.fetchall()
+    raw_data_2 = list(data_2[len(data_2) - 1])
+    return render_template('elements.html', write=keys[0], read=keys[1], time_1=raw_data_1[0], data_1=raw_data_1[2],
+                           time_2=raw_data_2[0], topic=raw_data_2[2], data_2=raw_data_2[3])
 
 
-class show_data(Resource):
+class ShowData(Resource):
     @staticmethod
     def get():
         parser = reqparse.RequestParser()
@@ -43,24 +78,30 @@ class show_data(Resource):
             return {"feeds_last": 'wrong api key'}
 
 
-class add_data(Resource):
+class AddData(Resource):
     @staticmethod
     def get():
         parser = reqparse.RequestParser()
-        parser.add_argument('write_api_key', type=str)
-        parser.add_argument('field', type=str)
+        parser.add_argument('api_key', type=str)
+        parser.add_argument('field1', type=str)
         data = parser.parse_args()
-        if str(data['write_api_key']).upper() == api_key_write:
+        if str(data['api_key']).upper() == api_key_write:
             conn = db_connect.connect()
             conn.execute("INSERT INTO sensor_values (created_at,field) VALUES(?, ?)",
-                         (str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')), data['field']))
+                         (str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')), data['field1']))
             return {"data": 'inserted'}
         else:
             return {"data": 'insertion error'}
 
 
-api.add_resource(show_data, '/feeds.json')
-api.add_resource(add_data, '/update', endpoint='update')
+api.add_resource(ShowData, '/feeds.json')
+api.add_resource(AddData, '/update', endpoint='update')
 
 if __name__ == '__main__':
-    app.run(host='192.168.1.6', port=5000)
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect(broker_url, broker_port)
+    client.subscribe(topic)
+    client.loop_start()
+    app.run()
